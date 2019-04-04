6e07074ba02e11aafef7d71a53fa8f768f3af1d7
Anna <anna-maks99@lenta.ru>
2019-04-04 20:54:38 +0300

    .

--- tic_tac_toe/views.py
+++ tic_tac_toe/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def play_tic_tac_toe(request, **kwargs):
-    pass
+    return render(request, 'tic-tac_toe/index.html')
 
 def show_settings(request, **kwargs):
     pass
